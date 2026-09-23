3412a40ef4cbfcc735558f57882089140ded0db3
Ebenezer Boye-Adeyinka <boyeadeyinkaebenezer@yahoo.com>
2026-09-23 11:42:41 +0100

    final touches

--- main.tf
+++ main.tf
@@ -183,3 +183,59 @@ resource "aws_default_security_group" "database_server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "test_frontend_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend server"
+  }
+}
+
+resource "aws_instance" "test_backend_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "backend server"
+  }
+}
+
+resource "aws_db_subnet_group" "test_rds_subnet_group" {
+  name       = "test_subnet"
+  subnet_ids = [aws_subnet.test_private_subnet.id, aws_subnet.test_database_subnet.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "test_db" {
+  allocated_storage    = 10
+  db_name              = "my_test_db"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "my_test_user"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
